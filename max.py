10d7e2794ff6097e4fb52b52e8c62055affc67fe
Jacques Patricks <60397613+Kanka-max@users.noreply.github.com>
2021-08-19 15:23:03 +0300

    Update max.py
    change image

--- max.py
+++ max.py
@@ -21,9 +21,9 @@ with st.beta_container():
   st.write("""
 Retail Analysis on inventory levels, customer behaviour and sales.""")
 url = "https://raw.githubusercontent.com/Kanka-max/ADS_REV_code/main/011%20lesson%20Retail%20%20analytics/dataonline.csv"
-img = "https://unsplash.com/photos/JCIJnIXv7SE?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink"
+#img = "https://unsplash.com/photos/JCIJnIXv7SE?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink"
 #img = Image.open("keg.jpg")
-st.sidebar.image(img, caption="Max Liquor")
+st.sidebar.image("https://images.app.goo.gl/95yHvVH9GfgzEkfk8", caption="Max Liquor")
 
 data = st.sidebar.file_uploader("Upload Transactions", type=['csv', 'xlsx'])
 
