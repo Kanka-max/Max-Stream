87974b9ac08bf49dc6d667ed33ca661d1d016f33
Kanka-max <jacquespatricksk@gmail.com>
2022-01-13 17:18:19 +0300

    image

--- max.py
+++ max.py
@@ -24,7 +24,7 @@ Retail analysis on inventory levels, customer behaviour and sales.""")
 url = "https://raw.githubusercontent.com/Kanka-max/ADS_REV_code/main/011%20lesson%20Retail%20%20analytics/dataonline.csv"
 #img = "https://unsplash.com/photos/JCIJnIXv7SE?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink"
 #img = Image.open("keg.jpg")
-st.sidebar.image("https://pin.it/6RkLHY0", caption="Savvy Tech", width = 200)
+st.image("https://pin.it/6RkLHY0", caption="Savvy Tech", width = 400)
 
 with st.beta_expander("About The Store"):
         st.write("""
